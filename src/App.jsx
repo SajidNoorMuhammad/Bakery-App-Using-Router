af8d900c6b9f16dab2c0d85e44db856a23f95516
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-09-06 01:01:44 +0500

    Add File.

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Bakery from './Pages/Bakery'
+import AboutUs from './Pages/AboutUs'
+import Contact from './Pages/Contact'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/bakery' element={<Bakery />}></Route>
+        <Route path='/about' element={<AboutUs />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
