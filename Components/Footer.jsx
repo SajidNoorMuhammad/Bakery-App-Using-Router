c323024430640d8804e6cc75757298b0c954e762
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-09-06 00:57:55 +0500

    Add File.

---
+++ Components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-yellow-400 py-6 mt-16">
+                <div className="container mx-auto text-center text-white">
+                    <p>&copy; 2024 Bakery Delights. All rights reserved.</p>
+                    <p>Follow us on:
+                        <a href="#" className="text-white ml-2 hover:text-gray-200">Instagram</a>,
+                        <a href="#" className="text-white ml-2 hover:text-gray-200">Facebook</a>,
+                        <a href="#" className="text-white ml-2 hover:text-gray-200">Twitter</a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
